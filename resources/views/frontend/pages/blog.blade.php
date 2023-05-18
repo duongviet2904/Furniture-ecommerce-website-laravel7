0d286dbea7fa2427cc78a430aa0ba1eaddd75c2a
HarryV <harry@mgpstore.com>
2023-05-19 01:19:26 +0700

    init

--- resources/views/frontend/pages/blog.blade.php
+++ resources/views/frontend/pages/blog.blade.php
@@ -31,7 +31,9 @@
                             <div class="col-lg-6 col-md-6 col-12">
                                 <!-- Start Single Blog  -->
                                 <div class="shop-single-blog">
-                                <img src="{{$post->photo}}" alt="{{$post->photo}}">
+                                    <div style="background-image: url('{{$post->photo}}')" class="shop-single-blog-image">
+
+                                    </div>
                                     <div class="content">
                                         <p class="date"><i class="fa fa-calendar" aria-hidden="true"></i> {{$post->created_at->format('d M, Y. D')}}
                                             <span class="float-right">
